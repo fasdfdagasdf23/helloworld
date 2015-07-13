a911e775f806fb86851b60d60771edc832406095
helloworlddddd <v741upsm7cnt@sute.jp>
2015-07-13 15:11:01 +0900

    loop while

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,8 +10,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
-            Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Hello World!!");
+                Console.ReadLine();
+            }
         }
     }
 }
